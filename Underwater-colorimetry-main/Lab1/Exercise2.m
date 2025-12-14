8938262efa6650bd6e9ec1a8094db1c81d980726
Color Lab <colorlab@iui.local>
2025-12-14 15:41:37 +0200

    Labs updates

--- Underwater-colorimetry-main/Lab1/Exercise2.m
+++ Underwater-colorimetry-main/Lab1/Exercise2.m
@@ -1,53 +1,32 @@
 % June 8, 2023
 % Underwater Colorimetry Course @ IUI Eilat
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 %%%                               Lab 1                                %%%
-%%%     Basic Image Formation and RAW Image Manipulation Exercises     %%%
-
-
-%%%                             Exercise 2                             %%%
-%%%                        Real Image Exercises                        %%%
-
+%       Basic Image Formation and RAW Image Manipulation Exercises       %
+%                                                                        %
+%                               Exercise 2                               %
+%                          Real Image Exercises                          %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
 clear all; close all;clc; warning off
 
-
-
 %% LOAD A LINEAR PNG IMAGE THAT HAS A Color Chart IN THE SCENE
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 %         Replace 'ConvertedImage.png' with your converted          %
 %                    images with a color chart.                     %
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 % You should have 3 .png images:                                    % 
 %    - Your_image_with_a_color_chart.png                            %
 %    - NikonImage.png                                               %
 %    - CanonImage.png                                               %
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 I = im2double(imread('ConvertedImage.png'));
-
 s = size(I);
 figure;imshow(I*2)
 title('Linear image','fontsize',20)
 
-
-
 %% LOAD COLOR CHART DATA
 % This example is given for a Macbeth ColorChecker.
-
 load MacbethColorCheckerData.mat
 
 % Location of grey patches in Macbeth ColorChecker.
@@ -56,9 +35,8 @@ neutralPatches = [4 1; 4 2; 4 3; 4 4; 4 5; 4 6];
 % Y value of grey patches of a Macbeth ColorChecker.
 neutralTarget = [89.57 57.76 35.15 19.44 9.08 3.43]./100; 
 
-% Modify these to work for your color chart, if different than a Macbeth
-% ColorChecker.
-
+% Modify these to work for your color chart, 
+% if different than a Macbeth ColorChecker.
 
 %% MAKE MASKS FOR THE PATCHES OF THE COLOR CHART
 % It places the masks for each patch on the image, and waits for the user 
@@ -69,8 +47,6 @@ neutralTarget = [89.57 57.76 35.15 19.44 9.08 3.43]./100;
 % for all patches.
 
 % In a Macbeth ColorChecker, the first patch is the Dark Skin (top left).
-
-
 
 % Generate masks using makeChartMask function
 masks = makeChartMask(3*I,chart,colors,20);
@@ -92,16 +68,12 @@ save(saveFile, 'masks');
 % Confirmation message
 fprintf('Masks struct has been saved to %s\n', saveFile);
 
-
-
 %% CHECK THE LINEARITY OF CAMERA RESPONSE  
 % In this cell we will validate image linearity.
-
 
 % RGB values extracted from patches 
 % Dimensions 3 x size(neutralPatches,1)
 neutralValues = getPatchValues(I,masks,neutralPatches,colors);
-
 
 % Plotting Y values vs. RGB values
 figure;
--- Underwater-colorimetry-main/Lab4/Lab4.m
+++ Underwater-colorimetry-main/Lab4/Lab4.m
@@ -1,40 +1,30 @@
 % January 12, 2025
 % Underwater Colorimetry Course @ IUI Eilat
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                               Lab 4                                 %%%
+%%%               Underwater Image Formation Exercises                  %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%                               Lab 4                                %%%
-%%%               Underwater Image Formation Exercises                 %%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%  LAB 4:  
-%        Exercise 1: Simulating DGK color chart underwater.
-%        Exercise 2: Direct signal (Dc) and Backscatter (Bc).
-%        Exercise 3: Optical comparison of different water types (Jerlov).
-%        Exercise 4: RGB's (Ic) as function of viewing distance (z).
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  LAB 4:                                                                 %
+%       Exercise 1: Simulating DGK color chart underwater.                % 
+%       Exercise 2: Direct signal (Dc) and Backscatter (Bc).              % 
+%       Exercise 3: Optical comparison of different water types (Jerlov). %
+%       Exercise 4: RGB's (Ic) as function of viewing distance (z).       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Loading the required data %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 clear all; close all; clc; 
-
 
 % Wavelength range: 400-700[nm]
 WL = 400:10:700;
 
-
 % Loading reflectences of DGK color chart
 load('Data/DGKColorChart.mat')
 Refl_spectra_DGK = importdata('Data/DGKcolorchart_reflectances.csv');
-
 
 % Loading the scattering coefficient b
 Jerlov_b = importdata('Data/Jerlov_b.csv');
@@ -43,34 +33,24 @@ Jerlov_Kd = importdata('Data/Jerlov_Kd.csv');
 % Loading the beam attenuation coefficient c
 Jerlov_c = importdata('Data/Jerlov_c.csv');
 
-
 % Loading camera sensitivities
 Cannon_Sc = importdata('Data/Canon_1Ds-Mk-II.csv');
-
 
 % Loading light spectrum
 light_D65 = importdata('Data/illuminant-D65.csv');
 % Interpolate data to wavelength range  
 light_spectra_D65 = interp1(light_D65.data(:,1),light_D65.data(:,2),WL);
 
-
-
-
-
 %% 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Exercise 1: Simulating DGK color chart underwater %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 % Set specific water type: 1 is the clearest, 8 is the most turbid.
 Water_Type = 3;
 % Vertical depth D: color chart depth below the sea surface
 Depth = 15;
 % Viewing distance z: distance between the observer and color chart
 Distance = 10;
-
-
 
 % Spectral attenuation coefficients for the choosen water type.
 % b is scattring coefficient
@@ -80,14 +60,11 @@ Kd = Jerlov_Kd(:,Water_Type);
 c = Jerlov_c(:,Water_Type);
 b = Jerlov_b(:,Water_Type);
 
-
 % Scaling parameters
 % D = 0
 % z = 0
 RGB_scale = get_UW_radiance(Refl_spectra_DGK.data(:,2:end)', light_spectra_D65, Kd, 0, c, 0, b, Cannon_Sc.data(:,2:end));
 white_scaling_value = RGB_scale(3,:);
-
-
 
 % Simulating RGB values given water type and viewing geometry
 [Ic, Dc, Bc] = get_UW_radiance(Refl_spectra_DGK.data(:,2:end)', light_spectra_D65, Kd, Depth, c, Distance, b, Cannon_Sc.data(:,2:end));
@@ -107,7 +84,6 @@ title(formattedTitle, 'FontSize', 18);
 subtitleText = sprintf('Depth and viewing distance are equal to zero');
 text(0.5, -0.05, subtitleText, 'Units', 'normalized', 'HorizontalAlignment', 'center', 'FontSize', 20);
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%
 %%% Underwater image %%%
 %%%%%%%%%%%%%%%%%%%%%%%%
@@ -118,18 +94,16 @@ title(formattedTitle, 'FontSize', 18);
 subtitleText = sprintf('Water type is: Jerlov %d', Water_Type);
 text(0.5, -0.05, subtitleText, 'Units', 'normalized', 'HorizontalAlignment', 'center', 'FontSize', 20);
 
-
 %%%%%%%%%%%%%%%%
 %%% Kd and c %%%
 %%%%%%%%%%%%%%%%
 figure;
 set(gcf, 'Color', 'w');
 
-subplot(1, 2, 1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Add here: a plot of Kd vs. wavelength %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+subplot(1, 2, 1)
 plot(WL, Kd, 'o--', 'LineWidth', 2)
 
 xlabel('$\mathrm{\lambda\ [nm]}$', 'Interpreter', 'latex', 'FontSize', 18)
@@ -138,13 +112,10 @@ title('$\mathrm{Diffuse\ downwelling\ attenuation\ coefficient\ -\ } K_d(\lambda
       'Interpreter', 'latex', 'FontSize', 20)
 set(gca, 'Color', 'w'); 
 
-
-
-subplot(1, 2, 2)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Add here: a plot of c vs. wavelength %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+subplot(1, 2, 2)
 plot(WL, c, 'o--', 'LineWidth', 2)
 
 xlabel('$\mathrm{\lambda [nm]}$', 'Interpreter', 'latex', 'FontSize', 18)
@@ -153,15 +124,10 @@ title('$\mathrm{Beam\ attenuation\ coefficient\ -\ } c(\lambda)$', ...
       'Interpreter', 'latex', 'FontSize', 20)
 set(gca, 'Color', 'w'); 
 
-
-
-
 %% 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Exercise 2: Direct signal (Dc) and Backscatter (Bc) %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 figure;
 Dc_scaled = Dc./white_scaling_value;
 montage({visualizeDGK(mat2gray(Ic_scaled)), ... 
@@ -175,22 +141,13 @@ title(formattedTitle, 'Interpreter', 'latex', 'FontSize', 15);
 subtitleText = sprintf('Water type is: Jerlov %d, Depth = %d[m], Viewing distance = %d[m]', Water_Type, Depth, Distance);
 text(0.5, -0.04, subtitleText, 'Units', 'normalized', 'HorizontalAlignment', 'center', 'FontSize', 15);
 
-
-
-
-
-
 %% 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Exercise 3: Comparing the 8 different Jerlov water types %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 % Initialize an image array to store the 8 images
 % Use a cell array for storing the images
 CC_array = cell(1, 8); 
-
-
 
 % Total signal (Ic) and Direc signal (Dc) comparison in different water types
 for i = 1:8
@@ -206,7 +163,9 @@ for i = 1:8
     CompDistance = 2;
 
     % Compute the underwater radiance
-    [Ic, Dc, Bc] = get_UW_radiance(Refl_spectra_DGK.data(:, 2:end)', light_spectra_D65, Kd, CompDepth, c, CompDistance, b, Cannon_Sc.data(:, 2:end));
+    [Ic, Dc, Bc] = get_UW_radiance(Refl_spectra_DGK.data(:, 2:end)', ... 
+        light_spectra_D65, Kd, CompDepth, c, CompDistance, ...
+        b, Cannon_Sc.data(:, 2:end));
 
     % Normalize the radiance values
     Ic_scaled = Ic./ white_scaling_value;
@@ -221,21 +180,15 @@ end
 Ic_montage = cat(4, Ic_array{:});
 Dc_montage = cat(4, Dc_array{:});
 
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Display the total signal Ic %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 figure;
 montage(Ic_montage, 'Size', [2 4]); 
 title('Comparsion of $I_c$ in 8 optically different water types from Jerlovs data-set', ...
     'Interpreter', 'latex','FontSize',15);
 text(0.5, -0.05, 'Up left - clearest waters ; Bottom right - most turbid waters', ...
     'Units', 'normalized', 'HorizontalAlignment', 'center', 'FontSize', 15)
-
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Display the direct signal Dc %%%
@@ -247,18 +200,10 @@ title('Comparsion of $D_c$ in 8 optically different water types from Jerlovs dat
 text(0.5, -0.05, 'Up left - clearest waters ; Bottom right - most turbid waters', ...
     'Units', 'normalized', 'HorizontalAlignment', 'center', 'FontSize', 15)
 
-
-
-
-
-
-
 %% 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Exercise 4: RGB's (Dc, Bc) as function of viewing distance (z) %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 % Set parameters
 Water_Type = 2;
 Depth = 5;
@@ -269,22 +214,14 @@ b = Jerlov_b(:, Water_Type);
 % Experiment with different patches
 patch = 3;
 
-
-
-
 figure;
 set(gcf, 'Color', 'w');
-
 for z = 0:50
-
     [Ic, Dc, Bc] = get_UW_radiance(Refl_spectra_DGK.data(:, 2:end)', light_spectra_D65, Kd, Depth, c, z, b, Cannon_Sc.data(:, 2:end));
     
-
-
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% Subplot for Red Channel %%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
     subplot(3, 1, 1)
 
     plot(z, Dc(patch, 1), 'xk', 'LineWidth', 2)
@@ -298,13 +235,10 @@ for z = 0:50
     title('$\mathrm{I_c,\ D_c,\ and\ B_c\ -\ Red\ channel\ as\ function\ of\ depth\ (D)}$', ...
           'Interpreter', 'latex', 'FontSize', 16)
     set(gca, 'Color', 'w'); 
-        
-    
-    
+         
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% Subplot for Green Channel %%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
     subplot(3, 1, 2)
 
     plot(z, Dc(patch, 2), 'xk', 'LineWidth', 2)
@@ -319,12 +253,9 @@ for z = 0:50
           'Interpreter', 'latex', 'FontSize', 16)
     set(gca, 'Color', 'w'); 
     
-
-
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%% Subplot for Blue Channel %%%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
     subplot(3, 1, 3)
 
     plot(z, Dc(patch, 3), 'xk', 'LineWidth', 2)
